0c1836d0f4eccd758e2e68e7402bac02deb09e3b
Hrithik Naha <naha.hrithkk@gmail.com>
2019-03-15 12:48:47 +0530

    Add describe block

--- server-testing/server.test.js
+++ server-testing/server.test.js
@@ -5,27 +5,30 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', function (done){
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect(function (res){
-            expect(res.body).toInclude({
-                error: "Page not found."
+describe('http tests', function(){
+    it('should return hello world response', function (done){
+        request(app)
+            .get('/')
+            .expect(404)
+            .expect(function (res){
+                expect(res.body).toInclude({
+                    error: "Page not found."
+                })
             })
-        })
-        .end(done);
+            .end(done);
+    })
+    
+    it('it should return my user fucnction', function (done){
+        request(app)
+            .get('/users')
+            .expect(200)
+            .expect(function (res){
+                expect(res.body).toInclude({
+                    name : 'Hrithik Naha',
+                    age : 20
+                })
+            })
+            .end(done);
+    })
 })
 
-it('it should return my user fucnction', function (done){
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect(function (res){
-            expect(res.body).toInclude({
-                name : 'Hrithik Naha',
-                age : 20
-            })
-        })
-        .end(done);
-})
--- utils.test.js
+++ utils.test.js
@@ -2,31 +2,39 @@ const expect = require("expect");
 
 const utils = require("./utils");
 
-it('should add two numbers', function(){
-    var res = utils.add(33,11);
-    expect(res).toBe(44);
-})
-
-it('should square the number', function(){
-    var res = utils.square(9);
-    expect(res).toBe(81);
-})
-
-it('should squatre the number ascynchronously', function(done){
-    utils.aSyncSquare(9, function(square){
-        expect(square).toBe(81);
-        done();
+describe('utils test', function(){
+    describe('#add', function(){
+        it('should add two numbers', function(){
+            var res = utils.add(33,11);
+            expect(res).toBe(44);
+        });
     });
-})
-
-it('should include the name', function(){
-    var res = utils.nameInclude({
-        Address : "Newtown",
-        Age : 20
-    },'Hrithik Naha')
-
-    expect(res).toInclude({
-        firstName: "Hrithik",
-        lastName: "Naha"
+    
+    describe('#square', function(){
+        it('should square the number', function(){
+            var res = utils.square(9);
+            expect(res).toBe(81);
+        })
+        
+        it('should squatre the number ascynchronously', function(done){
+            utils.aSyncSquare(9, function(square){
+                expect(square).toBe(81);
+                done();
+            });
+        })
+    })
+    
+    
+    it('should include the name', function(){
+        var res = utils.nameInclude({
+            Address : "Newtown",
+            Age : 20
+        },'Hrithik Naha')
+    
+        expect(res).toInclude({
+            firstName: "Hrithik",
+            lastName: "Naha"
+        })
     })
 })
+
